d5e7f8b2302a19e9cf75ba33c844288bb1b5063a
Quentin Flament <quentin.flament@hes-so.ch>
2022-10-15 14:58:08 +0200

    Nav Bar language selection

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,43 @@
-import React from 'react';
-import {Nav, Navbar, NavbarBrand, NavItem, NavLink} from "reactstrap";
+import React, {useEffect, useState} from 'react';
+import {
+    Nav, Navbar, NavbarBrand, NavItem, NavLink, DropdownMenu,
+    DropdownItem, ButtonDropdown, DropdownToggle, UncontrolledButtonDropdown
+} from "reactstrap";
 
 export function NavBar(){
+    const [language, setLanguage] = useState("en");
+
+    useEffect(() => {
+        const language = sessionStorage.getItem("language");
+
+        sessionStorage.setItem("language", language);
+
+    }, [language]);
    return (
        <div>
            <Navbar color={"light"}>
                <NavbarBrand href = "/">
                    VAST
                </NavbarBrand>
-               <Nav navbar>
-                   <NavItem>
-                       <NavLink href = "acuityTestScreen">
-                           Test Screen
-                       </NavLink>
-                       <NavLink href = "acuityTestController" target={"_blank"}>
-                           Controller Screen
-                       </NavLink>
-                   </NavItem>
-               </Nav>
+               <UncontrolledButtonDropdown>
+                   <DropdownToggle caret>Language</DropdownToggle>
+                   <DropdownMenu>
+                       <DropdownItem onClick={()=>setLanguage("fr")}>Français</DropdownItem>
+                       <DropdownItem onClick={()=>setLanguage("en")}>English</DropdownItem>
+                       <DropdownItem onClick={()=>setLanguage("po")}>Portugais</DropdownItem>
+                   </DropdownMenu>
+               </UncontrolledButtonDropdown>
+
+               {/*<Nav navbar>*/}
+               {/*    <NavItem>*/}
+               {/*        <NavLink href = "acuityTestScreen">*/}
+               {/*            Test Screen*/}
+               {/*        </NavLink>*/}
+               {/*        <NavLink href = "acuityTestController" target={"_blank"}>*/}
+               {/*            Controller Screen*/}
+               {/*        </NavLink>*/}
+               {/*    </NavItem>*/}
+               {/*</Nav>*/}
            </Navbar>
 
        </div>
