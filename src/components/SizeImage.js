e8526b0537d3f46590c4979871e1a8f7ef3188d7
Quentin Flament <quentin.flament@hes-so.ch>
2022-10-26 21:35:24 +0200

    Random Positionning image

--- src/components/SizeImage.js
+++ src/components/SizeImage.js
@@ -4,6 +4,30 @@ import React, {useState} from "react";
 export function SizeImage({ axe }){
     axe = (axe+1)*100;
     const [size, setSize] = useState(axe);
+
+    function randomAxe()  {
+        const min = 0;
+        const max = 4;
+        let rotation = min + Math.random() * (max - min);
+        rotation = parseInt(rotation);
+        switch (rotation){
+            case 0 :
+                console.log(0)
+                return 0;
+            case 1 :
+                console.log(90)
+                return "90";
+            case 2 :
+                console.log(180)
+                return "180";
+            case 3 :
+                console.log(270)
+                return "270";
+        }
+        console.log(rotation)
+        return rotation
+    }
+
     return (
         <div>
             <input
@@ -23,12 +47,14 @@ export function SizeImage({ axe }){
                     className="carteImg"
                     alt="Weiswampach Carte"
                     src={"https://www.ocnishop.cz/wp-content/uploads/2019/11/59577_602600_landolt_c_plastic_letter.jpg"}
-                    style={{ width: `${size}px` }}
+                    style={{ width: `${size}px` ,transform: `rotate(${randomAxe()}deg)`}}
                 />
             </div>
         </div>
     );
 
 }
+
+
 
 export default SizeImage
