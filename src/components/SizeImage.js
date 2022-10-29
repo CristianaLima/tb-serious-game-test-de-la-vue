368c842b33fa08012b9571b62fa1994fa26a1dce
Quentin Flament <quentin.flament@hes-so.ch>
2022-10-29 17:38:46 +0200

    Random Positionning image

--- src/components/SizeImage.js
+++ src/components/SizeImage.js
@@ -3,16 +3,23 @@ import React, {useState} from "react";
 export function SizeImage({ axe }){
     axe = (axe+1)*100;
     const [size, setSize] = useState(axe);
-    const [rotation, setRotation] = useState("0");
+    const [rotation, setRotation] = useState(0);
     const [array, setArray] = useState([0,1,2,3,0]);
 
 
 
-    function testValue({value}){
-        while (value == array[4]){
-            randomAxe()
+    function testValue() {
+        console.log("before if testValue "+rotation);
+        console.log(array[4]);
+        if(rotation === array[4]){
+            setRotation(randomAxe());
+            console.log("inside If "+rotation);
         }
-        return value;
+        console.log("after if testValue "+rotation);
+        array[4] = rotation;
+        console.log(array);
+        console.log(array[4]);
+        return rotation;
     }
 
     function randomAxe()  {
@@ -20,20 +27,21 @@ export function SizeImage({ axe }){
         const max = 4;
         let random = min + Math.random() * (max - min);
         random = parseInt(random);
-        console.log("before Switch Case"+random)
+        console.log("before Switch Case"+random);
         switch (random){
             case 0 :
-                testValue(random);
+                //setRotation(0)
                 return 0;
             case 1 :
-                testValue(random);
+                //setRotation(90)
                 return 90;
             case 2 :
-                testValue(random);
+                //setRotation(180)
                 return 180;
             case 3 :
-                testValue(random);
+                //setRotation(270)
                 return 270;
+
         }
     }
 
@@ -50,7 +58,7 @@ export function SizeImage({ axe }){
                     setSize(parseInt(value, 10));
                 }}
             />
-            <button onClick={()=>{setRotation(randomAxe())}}>
+            <button onClick={()=>{setRotation(testValue)}}>
                 Tournez le C
             </button>
             <div>
@@ -61,6 +69,8 @@ export function SizeImage({ axe }){
                     src={"https://www.ocnishop.cz/wp-content/uploads/2019/11/59577_602600_landolt_c_plastic_letter.jpg"}
                     style={{ width: `${size}px` ,transform: `rotate(${rotation}deg)`}}
                 />
+                <div>{rotation}</div>
+                <div>{array[4]}</div>
             </div>
         </div>
     );
