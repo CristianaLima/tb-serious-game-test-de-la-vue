f0ffab8b8b33237ed6fc4105018f4dc017e15af2
Quentin Flament <quentin.flament@hes-so.ch>
2022-10-29 16:14:53 +0200

    Random Positionning image

--- src/components/SizeImage.js
+++ src/components/SizeImage.js
@@ -1,31 +1,40 @@
 import React, {useState} from "react";
 
-
 export function SizeImage({ axe }){
     axe = (axe+1)*100;
     const [size, setSize] = useState(axe);
+    const [rotation, setRotation] = useState("0");
+    const [array, setArray] = useState([0,1,2,3,0]);
+
+
+
+    function testValue({value}){
+        while (value == array[4]){
+            randomAxe()
+        }
+        return value;
+    }
 
     function randomAxe()  {
         const min = 0;
         const max = 4;
-        let rotation = min + Math.random() * (max - min);
-        rotation = parseInt(rotation);
-        switch (rotation){
+        let random = min + Math.random() * (max - min);
+        random = parseInt(random);
+        console.log("before Switch Case"+random)
+        switch (random){
             case 0 :
-                console.log(0)
+                testValue(random);
                 return 0;
             case 1 :
-                console.log(90)
-                return "90";
+                testValue(random);
+                return 90;
             case 2 :
-                console.log(180)
-                return "180";
+                testValue(random);
+                return 180;
             case 3 :
-                console.log(270)
-                return "270";
+                testValue(random);
+                return 270;
         }
-        console.log(rotation)
-        return rotation
     }
 
     return (
@@ -41,13 +50,16 @@ export function SizeImage({ axe }){
                     setSize(parseInt(value, 10));
                 }}
             />
+            <button onClick={()=>{setRotation(randomAxe())}}>
+                Tournez le C
+            </button>
             <div>
                 <img
                     id="image"
                     className="carteImg"
                     alt="Weiswampach Carte"
                     src={"https://www.ocnishop.cz/wp-content/uploads/2019/11/59577_602600_landolt_c_plastic_letter.jpg"}
-                    style={{ width: `${size}px` ,transform: `rotate(${randomAxe()}deg)`}}
+                    style={{ width: `${size}px` ,transform: `rotate(${rotation}deg)`}}
                 />
             </div>
         </div>
