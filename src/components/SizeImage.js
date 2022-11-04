f41cd7a8bf9c02a79ecfbefe2b909d3700bc3dc4
Gwen <gwenaelle.gustin@students.hevs.ch>
2022-11-04 15:56:06 +0100

    refactor SizeImage.js renamming variable image reference

--- src/components/SizeImage.js
+++ src/components/SizeImage.js
@@ -1,110 +1,128 @@
 import React, {useEffect, useState} from "react";
 import {LS_C_SELECTED,  MAXREP} from "../views/App";
-import {algoSimulation} from "../algo/algoSimulation";
+import c from "../assets/c_picture.png";
 
+/**
+ *
+ * response: stock tour and angle given by the controller screen
+ * answer: true or false depend of real angle and the response
+ * angleArray: list of angle p
+ */
 export function SizeImage(){
-    const [testStatus, setTestStatus] = useState(0) //0 = not begins, 1 = begins, 2 = finish
+    const [response, setResponse] = useState({tour : 0, angle : 0}) //
+    const [answer, setAnswer] = useState(false);
+    const [angleArray] = useState(() => constructAngleArray())
+    const [rotation, setRotation] = useState(angleArray[0]);
     const [size, setSize] = useState(100);
-    const [rotation, setRotation] = useState(0);
-    const [answer, setAnswer] = useState(-1);
+    const [result, setResult] = useState(0);//0 = not begins, 1 = begins, other = [last size, result of the test]
 
-    let orientationArray;
-    let array2 = [];
-    let tour = 1;
-
-    const algo = new algoSimulation(MAXREP);
-
-    // Each time an answer is selected, next C appeared
+    /**
+     * Add a listener on local storage
+     * Stock the new value in response
+     */
     useEffect(() => {
-        constructOrientationArray();
-        setRotation(orientationArray[0])
-        console.log("actual axe: " + orientationArray[0])
-        localStorage.setItem(LS_C_SELECTED, JSON.stringify({tour: 0, axe:-1}))
+        localStorage.setItem(LS_C_SELECTED, JSON.stringify({tour: 1, angle:-1}))
         window.dispatchEvent(new Event("storage"));
         window.addEventListener("storage", e =>{
-                if (tour < MAXREP-1 ){
-                    setTestStatus(1); // test begins
-
-                    const newValue = JSON.parse(localStorage.getItem(LS_C_SELECTED))
-
-                    tour = newValue.tour;
-                    setRotation(orientationArray[tour]) //Change rotation
-
-                    //Test if the selected C is the correct one
-                    if(newValue.axe == orientationArray[tour-1]){
-                        setAnswer(1)
-                        setSize(algo.update(1)*100);
-                    }else{
-                        setAnswer(0)
-                        setSize(algo.update(0)*100);
-                    }
-
-                }else{
-                    console.log("Test finish, results is : " + algo.getResult())
-                    setSize(0);
-                    setTestStatus(2);
-                }
+                // Get info from local storage
+                setResponse(JSON.parse(localStorage.getItem(LS_C_SELECTED)))
             }
         );
     }, []);
 
-    //Constructing an array with equal part of each angle. Then shuffle it until there isn't two identical values in a row
-    function constructOrientationArray(){
+    /**
+     * Each time response is changed, this use effect can
+     * - do nothing if first tour
+     * - set result if test is finish
+     * - change size and angle of next C on other case
+     */
+    useEffect(() => {
+        switch (response.tour){
+            case 0 :
+                break;
+            case MAXREP :
+                setResult(size) // Last size is result
+                setSize(0); //C disappear
+                break;
+            default :{
+                // Test begins
+                setResult(1)
 
+                //Change size of C depend on answer correctness
+                if(response.angle == angleArray[response.tour-1]){
+                    setAnswer(true)
+                    if(size<50)
+                        setSize(size /1.4);
+                    else
+                        setSize(size /1.8)
+                }
+                else{
+                    setAnswer(false)
+                    if(size<100) {
+                        setSize(size * 1.8)
+                    }
+                }
+
+                // Rotate C
+                setRotation(angleArray[response.tour])
+            }
+        }
+    },[response]);
+
+
+    /**
+     * Constructing an array with equal part of each angle.
+     * Then shuffle it until there isn't two identical values in a row
+     */
+    function constructAngleArray(){
+        let array = [];
         //Constructing
         for (let i = 0; i < MAXREP; i++) {
             switch (i%4){
-                case 0 : array2.push(0);
+                case 0 : array.push(0);
                     break;
-                case 1 : array2.push(90);
+                case 1 : array.push(90);
                     break;
-                case 2 : array2.push(180);
+                case 2 : array.push(180);
                     break;
-                case 3 : array2.push(270);
+                case 3 : array.push(270);
                     break;
+                default : array.push(-1)
             }
         }
 
         //Shuffling
         let result = false;
 
-        while(result == false){
+        while(result === false){
             result = true;
-            array2.sort(() => Math.random() - 0.5);
+            array.sort(() => Math.random() - 0.5);
 
-            for (let i = 1; i < array2.length; i++) {
-                if(array2[i] == array2[i-1]){
+            for (let i = 1; i < array.length; i++) {
+                if(array[i] === array[i-1]){
                     result = false;
                 }
             }
         }
-        orientationArray = array2;
-        //console.log("Shuffled array : " + orientationArray);
+        return array
     }
 
     return (
         <div>
-            {testStatus!==2 ?
+            {result<=1 ?
                 <>
                     <img
                         id="image"
                         className="carteImg"
-                        alt="Weiswampach Carte"
-                        src={"https://www.ocnishop.cz/wp-content/uploads/2019/11/59577_602600_landolt_c_plastic_letter.jpg"}
+                        alt="C landolt"
+                        src={c}
                         style={{ width: `${size}px` ,transform: `rotate(${rotation}deg)`}}
                     />
-                    {testStatus===0 ? "" : <div>last answer was {answer===1? "true" : "false"}</div>}
-
+                    {result === 0 ? "" : <div>Last answer was {answer.toString()}</div>}
                 </>
-                :  <div>Test Finish</div>}
-            <div>
-
-            </div>
+                :  <div>Test Finish with result {Math.round(result)}</div>}
         </div>
     );
-
 }
-
-
 
 export default SizeImage
