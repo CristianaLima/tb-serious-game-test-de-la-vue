cd39c37b0c01c90e5213da0e8702461a370aa4cd
gwenaelle.gustin <gwenaelle.gustin@students.hevs.ch>
2022-11-10 21:26:35 +0100

    Synchronisation clean (students and tests)

--- src/config/SynchroFirebase.js
+++ src/config/SynchroFirebase.js
@@ -13,27 +13,28 @@ import {
     getAllTests,
 } from "./InitFirebase";
 
+/**
+ * Main method for synchronise data in local storage to Firebase
+ */
 export async function synchronise(){
-
-
-   await synchroniseStudents().then(r => synchroniseTest(r))
-    //await synchroniseTest();
-
-    /*for (let i = newStudents.length-1; i > -1; i--) {
-        if (newStudents[i].id !== ""){
-            newStudents = newStudents.filter((item, j) => j !== i)
-        }
-    }*/
+    // Synchronise students, test and clear local storage from pushed data
+   await synchroniseStudents().then(r => synchroniseTests(r)).then(() => clearLocalStorage())
 
     // Refresh data
-    //await stockDataInLocalStorage();
-
-    // Clear local storage from pushed data
-    //TODO: check that all element has a id from firebase before deletes
-    //localStorage.setItem(LS_NEW_STUDENTS, JSON.stringify([]));
-    //localStorage.setItem(LS_NEW_VISUALSTESTS, JSON.stringify([]));
+    await stockDataInLocalStorage();
 }
 
+/**
+ * Clear NEW values (student, visual test) in local storage
+ */
+function clearLocalStorage () {
+    localStorage.setItem(LS_NEW_STUDENTS, JSON.stringify([]));
+    localStorage.setItem(LS_NEW_VISUALSTESTS, JSON.stringify([]));
+}
+
+/**
+ * Get data (schools, students, tests) from Firebase and stock in local storage
+ */
 export async function stockDataInLocalStorage() {
     getAllSchools().then(s => localStorage.setItem(LS_SCHOOLS, JSON.stringify(s)));
     getAllStudents().then(s => localStorage.setItem(LS_STUDENTS, JSON.stringify(s)));
@@ -41,10 +42,12 @@ export async function stockDataInLocalStorage() {
     //TODO: if empty, error handling
 }
 
-export async function synchroniseStudents() {
+/**
+ * Push new students from local storage to Firebase
+ */
+async function synchroniseStudents() {
     //Get value in local storage
     let newStudents = JSON.parse(localStorage.getItem(LS_NEW_STUDENTS));
-    const newStudentsEdited = []
 
     if (newStudents != null) {
         for (let i = 0; i < newStudents.length; i++) {
@@ -54,54 +57,45 @@ export async function synchroniseStudents() {
                 class: newStudents[i].class,
                 dob: newStudents[i].dob,
                 idSchool: newStudents[i].idSchool
-            }).then(r => {
-                newStudentsEdited.push({...newStudents[i], idStudent: r.id})
-                newStudents[i] = {...newStudents[i], idStudent: r.id};
+            }).then(r => { // Set idStudent from Firebase
+                newStudents[i]={...newStudents[i], id : r.id};
                 console.log("Student " + newStudents[i].id + " added to Firebase")
             });
-            //console.log(newStudents)
         }
     }
-    console.log(newStudentsEdited)
-    return newStudentsEdited
+    return newStudents
 }
 
-export async function synchroniseTest(newStudentsEdited){
+/**
+ * Push new tests from local storage to Firebase
+ */
+async function synchroniseTests(newStudentsEdited){
     //Get value in local storage
     const newTests = JSON.parse(localStorage.getItem(LS_NEW_VISUALSTESTS));
 
     if (newTests != null){
         for (let i = 0; i < newTests.length; i++) {
-            console.log(newTests[i])
-            if (newTests[i].idStudent == undefined){
-                //const student = newStudentsEdited.filter(s => { return s.localIdStudent === newTests[i].localIdStudent})
-
-                const index = newStudentsEdited.findIndex((s) => {
+            // Edit idStudent if it was a new student (from form)
+            if (newTests[i].idStudent === undefined){
+                newTests[i].idStudent = newStudentsEdited.find((s) => {
                     return s.localId === newTests[i].localIdStudent
-                })
-
-                // Set the student's id from Firebase with the LOCALIDSTUDENT
-                // Reconstruct student to delete value "localId"
-                let testFb = {}
-                // Add new test to Firebase
-                addTest( {
-                    comprehension : newTests[i].comprehension,
-                    correction: newTests[i].correction,
-                    vaLe: newTests[i].vaLe,
-                    vaRe: newTests[i].vaRe,
-                    dateTest: newTests[i].dateTest,
-                    rounds: newTests[i].rounds,
-                    idTherapist: newTests[i].idTherapist,
-                    idStudent: newStudentsEdited[index].idStudent
-                }).then(
-                    r => {newTests[i]={...newTests[i], id : r.id};
-                        console.log("Test " + newTests[i].id + " added to Firebase")
-                    });
-
+                }).id
             }
-
-
-
+            // Add new test to Firebase
+            addTest( {
+                comprehension : newTests[i].comprehension,
+                correction: newTests[i].correction,
+                vaLe: newTests[i].vaLe,
+                vaRe: newTests[i].vaRe,
+                dateTest: newTests[i].dateTest,
+                rounds: newTests[i].rounds,
+                idTherapist: newTests[i].idTherapist,
+                idStudent: newTests[i].idStudent
+            }).then(
+                r => { //
+                    newTests[i]={...newTests[i], id : r.id};
+                    console.log("Test " + newTests[i].id + " added to Firebase")
+                });
         }
     }
 
