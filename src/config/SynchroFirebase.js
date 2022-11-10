6dc7cd7a41e8a1cc51584f8e3664e787ac6c7342
gwenaelle.gustin <gwenaelle.gustin@students.hevs.ch>
2022-11-10 16:43:57 +0100

    first version of synchro working

--- src/config/SynchroFirebase.js
+++ src/config/SynchroFirebase.js
@@ -1,5 +1,4 @@
 import {
-    LS_CURRENT_THERAPIST,
     LS_NEW_STUDENTS,
     LS_NEW_VISUALSTESTS,
     LS_SCHOOLS,
@@ -11,30 +10,28 @@ import {
     addTest,
     getAllSchools,
     getAllStudents,
-    getAllTests, getSchoolById,
-    getStudentById
+    getAllTests,
 } from "./InitFirebase";
 
 export async function synchronise(){
-    //Get value in local storage
-    let newStudents = JSON.parse(localStorage.getItem(LS_NEW_STUDENTS));
 
-    await synchroniseStudent(newStudents);
-    await synchroniseTest(newStudents);
 
-    // Refresh data
-    await stockDataInLocalStorage();
+   await synchroniseStudents().then(r => synchroniseTest(r))
+    //await synchroniseTest();
 
-    // Clear local storage from pushed data
-    //TODO: check that all element has a id from firebase before deletes
-
-    for (let i = newStudents.length-1; i > -1; i--) {
+    /*for (let i = newStudents.length-1; i > -1; i--) {
         if (newStudents[i].id !== ""){
             newStudents = newStudents.filter((item, j) => j !== i)
         }
-    }
-    localStorage.setItem(LS_NEW_STUDENTS, JSON.stringify(newStudents));
-    localStorage.setItem(LS_NEW_VISUALSTESTS, JSON.stringify([]));
+    }*/
+
+    // Refresh data
+    //await stockDataInLocalStorage();
+
+    // Clear local storage from pushed data
+    //TODO: check that all element has a id from firebase before deletes
+    //localStorage.setItem(LS_NEW_STUDENTS, JSON.stringify([]));
+    //localStorage.setItem(LS_NEW_VISUALSTESTS, JSON.stringify([]));
 }
 
 export async function stockDataInLocalStorage() {
@@ -44,59 +41,67 @@ export async function stockDataInLocalStorage() {
     //TODO: if empty, error handling
 }
 
-export async function synchroniseStudent(newStudents){
-    // Synchronise added student
-    if (newStudents != null){
-        for (let i = 0; i < newStudents.length; i++) {
-                // Reconstruct student to delete value "localId"
-                let studentToPush = {
-                    fullName: newStudents[i].fullName,
-                    class: newStudents[i].class,
-                    dob: newStudents[i].dob,
-                    idSchool: newStudents[i].idSchool
-                };
-                // Add new student to Firebase and set the id
-                addStudent(studentToPush).then(r => {
-                    newStudents[i]={...newStudents[i], id : r.id};
-                    console.log("Student " + newStudents[i].id + " added to Firebase")
-                    });
+export async function synchroniseStudents() {
+    //Get value in local storage
+    let newStudents = JSON.parse(localStorage.getItem(LS_NEW_STUDENTS));
+    const newStudentsEdited = []
 
+    if (newStudents != null) {
+        for (let i = 0; i < newStudents.length; i++) {
+            // Add new student to Firebase and set the id
+            await addStudent({
+                fullName: newStudents[i].fullName,
+                class: newStudents[i].class,
+                dob: newStudents[i].dob,
+                idSchool: newStudents[i].idSchool
+            }).then(r => {
+                newStudentsEdited.push({...newStudents[i], idStudent: r.id})
+                newStudents[i] = {...newStudents[i], idStudent: r.id};
+                console.log("Student " + newStudents[i].id + " added to Firebase")
+            });
+            //console.log(newStudents)
         }
     }
-
+    console.log(newStudentsEdited)
+    return newStudentsEdited
 }
-export async function synchroniseTest(newStudents){
+
+export async function synchroniseTest(newStudentsEdited){
     //Get value in local storage
     const newTests = JSON.parse(localStorage.getItem(LS_NEW_VISUALSTESTS));
-    const therapist = JSON.parse(localStorage.getItem(LS_CURRENT_THERAPIST));
 
-    // Synchronise test done
-    //TODO: test when test page --> see working school ref in synchroniseStudent
     if (newTests != null){
         for (let i = 0; i < newTests.length; i++) {
-            if (newTests[i].idStudent == ""){
+            console.log(newTests[i])
+            if (newTests[i].idStudent == undefined){
+                //const student = newStudentsEdited.filter(s => { return s.localIdStudent === newTests[i].localIdStudent})
+
+                const index = newStudentsEdited.findIndex((s) => {
+                    return s.localId === newTests[i].localIdStudent
+                })
+
                 // Set the student's id from Firebase with the LOCALIDSTUDENT
-                newTests[i].idStudent = newStudents.find((s) => {
-                    return s.localIdStudent === newTests[i].localIdStudent
-                }).id
+                // Reconstruct student to delete value "localId"
+                let testFb = {}
+                // Add new test to Firebase
+                addTest( {
+                    comprehension : newTests[i].comprehension,
+                    correction: newTests[i].correction,
+                    vaLe: newTests[i].vaLe,
+                    vaRe: newTests[i].vaRe,
+                    dateTest: newTests[i].dateTest,
+                    rounds: newTests[i].rounds,
+                    idTherapist: newTests[i].idTherapist,
+                    idStudent: newStudentsEdited[index].idStudent
+                }).then(
+                    r => {newTests[i]={...newTests[i], id : r.id};
+                        console.log("Test " + newTests[i].id + " added to Firebase")
+                    });
+
             }
 
-            // Reconstruct student to delete value "localId"
-            let testFb = {
-                comprehension : newTests[i].comprehension,
-                correction: newTests[i].correction,
-                vaLe: newTests[i].vaLe,
-                vaRe: newTests[i].vaRe,
-                dateTest: newTests[i].dateTest,
-                rounds: newTests[i].rounds,
-                idStudent: newTests[i].idStudent,
-                idTherapist: therapist.id
-            }
-            // Add new test to Firebase
-            addTest(testFb).then(
-                r => {newTests[i]={...newTests[i], id : r.id};
-                    console.log("Test " + newTests[i].id + " added to Firebase")
-                });
+
+
         }
     }
 
