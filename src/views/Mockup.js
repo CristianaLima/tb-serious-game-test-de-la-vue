eebc1e34ef1b581113a51adacdcaa6be2afec566
gwenaelle.gustin <gwenaelle.gustin@students.hevs.ch>
2022-11-20 17:19:50 +0100

    localstrorage for mockup

--- src/views/Mockup.js
+++ src/views/Mockup.js
@@ -1,10 +1,54 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {NavBar} from "../components/NavBar";
 import { useNavigate } from "react-router-dom"
+import {LS_CURRENT_THERAPIST, LS_NEW_SCHOOLS, LS_NEW_STUDENTS, LS_NEW_VISUALSTESTS} from "./App";
+import {getTherapistById} from "../config/InitFirebase";
+import {stockDataInLocalStorage} from "../config/SynchroFirebase";
 
 
 export function Mockup(){
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+    // Load local storage if exist or initialise it
+    const [newSchools] = useState(() => {
+        return JSON.parse(localStorage.getItem(LS_NEW_SCHOOLS));
+    });
+    const [newStudents] = useState(() => {
+        return JSON.parse(localStorage.getItem(LS_NEW_STUDENTS));
+    });
+    const [newTests] = useState(() => {
+        return JSON.parse(localStorage.getItem(LS_NEW_VISUALSTESTS));
+    });
+
+
+
+    useEffect(() => {
+        // If internet connection, load data from Firebase
+        //TODO: double with navbar
+        let condition = navigator.onLine ? 'online' : 'offline';
+        if (condition === 'online') {
+            fetch('https://www.google.com/', { // Check for internet connectivity
+                mode: 'no-cors',
+            })
+                .then(() => {
+                    getTherapistById("X6ITtB97ZhCqf4Uw3yhH").then(t => localStorage.setItem(LS_CURRENT_THERAPIST, JSON.stringify(t)));
+                    stockDataInLocalStorage().then(() => console.log("Data load"))
+                }).catch(() => {
+            }  )
+        }
+        // Initialize new array in local storage if first opening
+        if (newSchools === null){
+            console.log("schools null")
+            localStorage.setItem(LS_NEW_SCHOOLS, JSON.stringify([]));
+        }
+        if (newStudents === null){
+            console.log("students null")
+            localStorage.setItem(LS_NEW_STUDENTS, JSON.stringify([]));
+        }
+        if (newTests === null){
+            console.log("tests null")
+            localStorage.setItem(LS_NEW_VISUALSTESTS, JSON.stringify([]));
+        }
+    }, []);
 
     return(
         <div className="Home">
