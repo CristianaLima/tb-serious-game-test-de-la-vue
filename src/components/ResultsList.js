429d4c399a5f452ea316bf71245377a411f364d5
Quentin Flament <quentin.flament@hes-so.ch>
2022-11-26 15:10:37 +0100

    Export Data from Table

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -5,10 +5,73 @@ import {
     LS_VISUALSTESTS
 } from "../views/App";
 import {getSchoolNameFromLS, getStudentFromLS} from "../config/SearchLocalStorage";
+import {Row} from "reactstrap";
 
-export function ResultsList(){
+export function ResultsList() {
     const [tests] = useState(JSON.parse(localStorage.getItem(LS_VISUALSTESTS)));
     const [newTests] = useState(JSON.parse(localStorage.getItem(LS_NEW_VISUALSTESTS)));
+
+    function tableToCSV() {
+
+        // Variable to store the final csv data
+        var csv_data = [];
+
+        // Get each row data
+        var rows = document.getElementsByTagName('tr');
+        for (var i = 0; i < rows.length; i++) {
+
+            // Get each column data
+            var cols = rows[i].querySelectorAll('td,th');
+
+            // Stores each csv row data
+            var csvrow = [];
+            for (var j = 0; j < cols.length; j++) {
+
+                // Get the text data of each cell
+                // of a row and push it to csvrow
+                csvrow.push(cols[j].innerHTML);
+            }
+
+            // Combine each column value with comma
+            csv_data.push(csvrow.join(";"));
+        }
+        console.log(csv_data);
+        // Combine each row data with new line character
+        csv_data = csv_data.join('\n');
+
+        // Call this function to download csv file
+        downloadCSVFile(csv_data);
+
+    }
+
+    function downloadCSVFile(csv_data) {
+
+        // Create CSV file object and feed
+        // our csv_data into it
+        var CSVFile = new Blob([csv_data], {
+            type: "text/csv"
+        });
+
+        // Create to temporary link to initiate
+        // download process
+        var temp_link = document.createElement('a');
+
+        // Download csv file
+        temp_link.download = "ExportResult.csv";
+        var url = window.URL.createObjectURL(CSVFile);
+        temp_link.href = url;
+
+        // This link should not be displayed
+        temp_link.style.display = "none";
+        document.body.appendChild(temp_link);
+
+        // Automatically click the link to
+        // trigger download
+        temp_link.click();
+        document.body.removeChild(temp_link);
+
+    }
+
 
     function TestsFromFirebase() {
         if (tests.length > 0) {
@@ -32,7 +95,14 @@ export function ResultsList(){
 
     function TableConstruction({theadData, tbodyData}) {
         return (
+
             <div>
+                <Row className="row-cols-lg-auto g-3 align-items-center"
+                     style={{ display: "flex", justifyContent: "end", alignItems: "flex-end"}}>
+                    <button type="button" className="btn btn-primary" onClick={()=>tableToCSV()}>
+                        Export
+                    </button>
+                </Row>
                 <input type="text" id="myInput" placeholder="Search for..."
                        title="Type in a name"></input>
             <table className="table">
